c1b003ba657e5f99280ada5f74390588a1a398c2
MYKOLL DANIEL CUNHA GUIMARAES <mykoll.dcguimaraes@senacsp.edu.br>
2024-10-07 20:26:29 -0300

    Criacao de token de sessao ao autenticar usuario

--- service/AuthService.php
+++ service/AuthService.php
@@ -1,5 +1,7 @@
 <?php
 
+session_start();
+
 require_once '../model/Usuario.php';
 require_once '../dao/UsuarioDAO.php';
 
@@ -15,8 +17,8 @@ switch($type)
         break;
 }
 
-
-function handlerRegistration(){
+function handlerRegistration()
+{
     // Receber os dados vindos por input do HTML
     $nome = filter_input(INPUT_POST, "new_nome");
     $email = filter_input(INPUT_POST, "new_email", FILTER_SANITIZE_EMAIL);
@@ -24,23 +26,35 @@ function handlerRegistration(){
     $confirm_password = filter_input(INPUT_POST, "confirm_password");
 
     // Verificar os dados informados
-    if(!$email || !$nome || !$password){
+    if(!$email || !$nome || !$password) {
         echo "Dados de input inválidos";
         return;
     }
 
-    if($password !== $confirm_password){
-        echo "Senhas não correspondem.";
+    if($password !== $confirm_password) {
+        echo "Senhas incompatíveis.";
         return;
-    }
+    }    
 
     // Etapa de segurança: criação da senha segura e geração do token
-    // Criação do Usuário no Banco de Dados
-    $usuario = new Usuario(null, $nome, $password, $email, null);
-    $usuarioDAO = new UsuarioDAO();
+    $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+    $token = bin2hex(random_bytes(25));
 
-    $usuarioDAO->create($usuario);
+    // Criação do Usuário no Banco de Dados
+    $usuario = new Usuario(null, $nome, $hashed_password, $email, $token);
+    $usuarioDAO = new UsuarioDAO();
+   
     // Redirecionar para a página do index
+    if($usuarioDAO->create($usuario)) 
+    {
+        $_SESSION['token'] = $token;
+        header('Location: ../views/index.php');
+        exit();
+    } else 
+    {
+        echo "Erro ao registrar no banco de dados";
+        exit();
+    }
 }
 
 ?>
